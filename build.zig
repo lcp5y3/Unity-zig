d577dd09adf85b1f59f03f9aa233b2aca5f23b7f
tanche <oruz@mailz.org>
2024-12-11 21:30:08 +0100

    correctly format build.zig

--- build.zig
+++ build.zig
@@ -3,25 +3,25 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const flags = [_][]const u8 {
-            "-std=c11",
-            "-Wcast-align",
-            "-Wcast-qual",
-            "-Wconversion",
-            "-Wexit-time-destructors",
-            "-Wglobal-constructors",
-            "-Wmissing-noreturn",
-            "-Wmissing-prototypes",
-            "-Wno-missing-braces",
-            "-Wold-style-cast",
-            "-Wshadow",
-            "-Wweak-vtables",
-            "-Werror",
-            "-Wall",
+    const flags = [_][]const u8{
+        "-std=c11",
+        "-Wcast-align",
+        "-Wcast-qual",
+        "-Wconversion",
+        "-Wexit-time-destructors",
+        "-Wglobal-constructors",
+        "-Wmissing-noreturn",
+        "-Wmissing-prototypes",
+        "-Wno-missing-braces",
+        "-Wold-style-cast",
+        "-Wshadow",
+        "-Wweak-vtables",
+        "-Werror",
+        "-Wall",
     };
 
     const unity_dep = b.dependency("Unity", .{});
-    const unity_lib= b.addStaticLibrary(.{
+    const unity_lib = b.addStaticLibrary(.{
         .name = "unity",
         .target = target,
         .optimize = optimize,
@@ -35,7 +35,4 @@ pub fn build(b: *std.Build) void {
     unity_lib.linkLibC();
 
     b.installArtifact(unity_lib);
-
 }
-
-
